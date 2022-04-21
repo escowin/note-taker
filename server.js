67ad054e1f6e33447f5a05b38408a79a0579cc16
escowin <edwin@escowinart.com>
2022-04-21 13:01:53 -0500

    localhost:3001 setup

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const express = require('express');
+// runs on localhost:3001
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`local server running on PORT ${PORT}`)
+})
