50370d1c455c111b55eb67273722f1aa91afc2ab
escowin <edwin@escowinart.com>
2022-11-05 11:31:45 -0500

    outlined plan

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("the best note taking app in all of open source");
+const fs = require('fs');
+const express = require('express');
+
+// html routes
+// - GET /notes returns notes.html
+// - GET * returns index.html
+
+// api routes
+// - GET /api/notes reads db.json, returns all saved notes as json
+// - POST /api/notes adds note to db.json, returns new note to client. each note needs a unique id when saved.
+// - DELETE /api/notes/:id recieves query paramter, then deletes a note from db.json based on its unique id
+
+// deploy application
+
+function noteTaker() {
+    let date = new Date().getFullYear();
+    console.log(`
+    note-taker \u00A9 ${date} Edwin M. Escobar
+    https://github.com/escowin/note-taker
+    `);
+}
+
+noteTaker();
