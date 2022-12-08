39c0356903a75349892846b0a36dc4f701cf4911
escowin <edwin@escowinart.com>
2022-12-07 23:20:23 -0600

    post insomnia test, repeated in localhost

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,6 @@
 // ::route:: /api/noteRoutes
 const router = require('express').Router();
-// const { findById } = require('../../lib/notes');
+const { createNote } = require('../../lib/notes');
 const notes = require("../../db/db.json");
 
 // CRUD
@@ -12,32 +12,16 @@ router.get('/notes', (req, res) => {
 });
 
 // - post a note. note will have an id
-// router.post('/notes', (req, res) => {
-//     req.body.id = notes.length.toString()
-//     if(!validated(req.body) {
-//         res.status(400).send("note improperly formatted")
-//     })
-// });
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNote(req.body, notes);
+    res.json(note);
+});
+
+// edit notes
+// router.update(){};
 
 // - delete a note based on its id
-
-// **previous unmodularized code**
-// router.get('/api/notes/:id', (req, res) => {
-//     const result = findById(req.params.id, notes)
-//     if (result) {
-//         res.json(result);
-//     } else {
-//         res.send(404);
-//     }
-
-//     // console.log("js:65, getting note by id | " + result);
-// });
-
-// - POST /api/notes adds note to db.json, returns new note to client. each note needs a unique id when saved.
-// router.post('/api/notes', (req, res) => {
-//     console.log("js:70 " + (req.body.id = notes.length.toString()));
-// });
-// - DELETE /api/notes/:id recieves query paramter, then deletes a note from db.json based on its unique id
-
+// router.delete(){};
 
 module.exports = router;
