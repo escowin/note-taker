f40185366ad5c7a90e69d9206de039717d1d7732
escowin <edwin@escowinart.com>
2023-06-11 13:21:04 -0500

    used for loops to push utf-16 code units into characters array

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,28 @@
-const notes = require("../db/db.json")
+const notes = require("../db/db.json");
+
+const characterSet = (array) => {
+  // the characters array is made alphanumeric string objects by iterating through the specified ranges of UTF-16 code units
+  //  A - Z
+  for (let i = 65; i <= 90; i++) {
+    array.push(String.fromCharCode(i));
+  }
+  //  a - z
+  for (let i = 97; i < 122; i++) {
+    array.push(String.fromCharCode(i));
+  }
+  //  0 - 9
+  for (let i = 48; i <= 57; i++) {
+    array.push(String.fromCharCode(i));
+  }
+};
 
 function generateId(req) {
-    let id = req.length
-    console.log(id)
+  let indexValue = req.length;
+  const characters = [];
+  const regex = /[A-Za-z0-0]/g;
+  const unicodeRange = "\u0000-\u007F";
+  characterSet(characters);
+  console.log(characters);
 }
 
-generateId(notes)
+generateId(notes);
