90605212d4e228741eaf4f59bb08aae7b3439c15
escowin <edwin@escowinart.com>
2023-06-11 17:46:37 -0500

    imported generateId helper into noteRoutes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,51 +1,52 @@
 // ::route:: /api/noteRoutes
-const router = require('express').Router();
-const { createNote } = require('../../lib/notes');
+const router = require("express").Router();
+const { createNote } = require("../../lib/notes");
 const notes = require("../../db/db.json");
+const { generateId } = require("../../utils/helpers");
 
 // CRUD
 // - get all notes
-router.get('/notes', (req, res) => {
-    let result = notes
-    res.json(result);
+router.get("/notes", (req, res) => {
+  let result = notes;
+  res.json(result);
 });
 
 // - post a note. note will have an id
-router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const note = createNote(req.body, notes);
-    res.json(note);
+router.post("/notes", (req, res) => {
+  req.body.id = generateId();
+  const note = createNote(req.body, notes);
+  res.json(note);
 });
 
 router.get("/notes/:id", (req, res) => {
-    let id = req.params.id
-    let result = notes[id];
-    console.log(result);
-    res.json(result)
-})
+  let id = req.params.id;
+  let result = notes[id];
+  console.log(result);
+  res.json(result);
+});
 
-// - edit a note | will require note.id 
+// - edit a note | will require note.id
 router.put("/notes/:id", (req, res) => {
-    const id = req.params.id;
-    const update = req.body;
-    notes[id] = {...update, id};
-    // updated note is sent back
-    res.json(notes[id])
+  const id = req.params.id;
+  const update = req.body;
+  notes[id] = { ...update, id };
+  // updated note is sent back
+  res.json(notes[id]);
 });
 
 // - delete a note based on its id
 router.delete("/notes/:id", (req, res) => {
-    const id = req.params.id;
-    const note = notes[id];
-    if (!note) {
-        return res.status(404).json({ error: "note note found"})
-    }
+  const id = req.params.id;
+  const note = notes[id];
+  if (!note) {
+    return res.status(404).json({ error: "note note found" });
+  }
 
-    // delete is a javascript keyword. this removes the specified note object from the notes array
-    delete notes[id];
-    res.json({ message: "note deleted"})
+  // delete is a javascript keyword. this removes the specified note object from the notes array
+  delete notes[id];
+  res.json({ message: "note deleted" });
 
-    // note: deleting object leaves a null value. need to rewrite db.json to remove null values & trailing commas
+  // note: deleting object leaves a null value. need to rewrite db.json to remove null values & trailing commas
 });
 
 module.exports = router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -5,31 +5,32 @@ const charRanges = [
   { min: 97, max: 122 }, // lowercase
   { min: 48, max: 57 }, // numeric
 ];
-const idLength = 6;
+const idLength = 8;
 
 function generateCharacterRange(min, max) {
   // iterates through the set range to push UTF-16 code units in `characters` array
   for (let i = min; i <= max; i++) {
     characters.push(String.fromCharCode(i));
   }
-  return;
 }
 
-function generateId(req) {
+function generateId() {
   const idArray = [];
-  let indexValue = req.length;
   charRanges.forEach((set) => generateCharacterRange(set.min, set.max));
-  //   id is 6 characters long
   for (let i = 0; i < idLength; i++) {
     const charIndex = Math.floor(Math.random() * characters.length);
     const charValue = characters[charIndex];
     idArray.push(charValue);
   }
 
-  //   concatenates the array of characters that make up the id
+  //   id is made by contatenating concatenates the array of characters that make up the id
   const id = idArray.join("");
-  console.log(id)
-  console.log(req)
+  const duplicateId = notes.some((note) => note.id === id);
+  if (duplicateId) {
+    return generateId();
+  }
+
+  return id;
 }
 
-generateId(notes);
+module.exports = { generateId };
