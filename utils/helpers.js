c2eebc393f97b8aae05bdcd8f82a5181243a97a2
escowin <edwin@escowinart.com>
2023-06-11 16:25:42 -0500

    created a random id 6 characters in length

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,6 +5,7 @@ const charRanges = [
   { min: 97, max: 122 }, // lowercase
   { min: 48, max: 57 }, // numeric
 ];
+const idLength = 6;
 
 function generateCharacterRange(min, max) {
   // iterates through the set range to push UTF-16 code units in `characters` array
@@ -15,9 +16,20 @@ function generateCharacterRange(min, max) {
 }
 
 function generateId(req) {
+  const idArray = [];
   let indexValue = req.length;
   charRanges.forEach((set) => generateCharacterRange(set.min, set.max));
-  console.log(characters);
+  //   id is 6 characters long
+  for (let i = 0; i < idLength; i++) {
+    const charIndex = Math.floor(Math.random() * characters.length);
+    const charValue = characters[charIndex];
+    idArray.push(charValue);
+  }
+
+  //   concatenates the array of characters that make up the id
+  const id = idArray.join("");
+  console.log(id)
+  console.log(req)
 }
 
 generateId(notes);
