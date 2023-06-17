e144fcf2d164b1baff0682088d7218191cd5ba82
escowin <edwin@escowinart.com>
2023-06-17 18:34:36 -0500

    used .splice() to remove note object. api routes fully restful

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,7 @@
 // ::route:: /api/noteRoutes
 const router = require("express").Router();
-const { createNote, updateNote } = require("../../lib/notes");
+const { createNote, updateNotes } = require("../../lib/notes");
 const { notes } = require("../../db/db.json");
-const { generateId } = require("../../utils/helpers");
 
 // CRUD
 // - get all notes
@@ -13,7 +12,6 @@ router.get("/notes", (req, res) => {
 
 // - post a note. note will have an id
 router.post("/notes", (req, res) => {
-  req.body.id = generateId();
   const note = createNote(req.body, notes);
   res.json(note);
 });
@@ -31,30 +29,27 @@ router.get("/notes/:id", (req, res) => {
 router.put("/notes/:id", (req, res) => {
   const id = req.params.id;
   const update = req.body;
-  let noteIndex = notes.findIndex(note => note.id === id);
+  let noteIndex = notes.findIndex((note) => note.id === id);
   if (!noteIndex) {
     return res.status(404).json({ error: "note not found" });
   }
-
   notes[noteIndex] = { ...update, id };
-  // updated note variable is sent back as a response. to do: re-write db.json
-  updateNote(notes)
+  updateNotes(notes);
   res.json(notes[noteIndex]);
 });
 
 // - delete a note based on its id
 router.delete("/notes/:id", (req, res) => {
   const id = req.params.id;
-  const note = notes.find(note => note.id === id);
+  const note = notes.findIndex((note) => note.id === id);
   if (!note) {
-    return res.status(404).json({ error: "note note found" });
+    return res.status(404).json({ error: "note not found" });
   }
 
-  // delete is a javascript keyword. note object is deleted from db.json
-  delete note;
+  // notes is updated by removing one note object through .splice(). this new notes array rewrites db.json
+  notes.splice(note, 1);
+  updateNotes(notes);
   res.json({ message: "note deleted" });
-
-  // note: deleting object leaves a null value. need to rewrite db.json to remove null values & trailing commas
 });
 
 module.exports = router;
