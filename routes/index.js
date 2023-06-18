1fd6fb5c80afbf3bf50b3c5a078d1d51a41ab581
escowin <edwin@escowinart.com>
2023-06-18 12:30:38 -0500

    streamlined server by simplifying how route are imported

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const apiRoutes = require('./apiRoutes');
+const htmlRoutes = require('./htmlRoutes');
+
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error</h1>');
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-const express = require('express');
-
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const express = require("express");
 
 // port set up for staging, and server instantiation
 const PORT = process.env.PORT || 3001;
-// instantiates server
 const app = express();
 
 // middleware | converts raw data sent over http into a json object.
@@ -14,18 +10,13 @@ app.use(express.urlencoded({ extended: false }));
 // - takes incoming POST json-formatted data & parses it into js req.body object
 app.use(express.json());
 // - allows frontend html scripts to run by making directory files readily available
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(require("./routes"));
 
-
-// ::middleware routing::
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
-// server routes
+// server
 app.listen(PORT, () => {
-    let date = new Date().getFullYear();
-
-    console.log(`    note-taker \u00A9 ${date} Edwin M. Escobar
+  let date = new Date().getFullYear();
+  console.log(`    note-taker \u00A9 ${date} Edwin M. Escobar
     https://github.com/escowin/note-taker
     server now on port ${PORT}
     http://localhost:${PORT}/`);
