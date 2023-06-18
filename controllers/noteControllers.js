64fceb542ad2168838151804b5a2d6c1755be116
escowin <edwin@escowinart.com>
2023-06-18 12:00:46 -0500

    consolidated controller functions as one object

--- controllers/noteControllers.js
+++ controllers/noteControllers.js
@@ -3,62 +3,64 @@ const path = require("path");
 const { notes } = require("../db/db.json");
 const { generateId } = require("../utils/helpers");
 
-// crud functions used in express routes
-function getAllNotes(req, res) {
-  let result = notes;
-  res.json(result);
-}
+// api endpoint crud methods used within express methods
+const noteController = {
+  // called in .get()
+  getAllNotes(req, res) {
+    let result = notes;
+    res.json(result);
+  },
+  getNote(req, res) {
+    const id = req.params.id;
+    const note = notes.find((note) => note.id === id);
+    if (!note) {
+      return res.status(404).json({ error: "note not found" });
+    }
+    res.json(note);
+  },
+  // called in .post()
+  createNote({ body }, res) {
+    body.id = generateId();
+    const note = body;
+    notes.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify({ notes }, null, 2)
+    );
+    res.json(note);
+  },
+  // called in .post()
+  updateNote(req, res) {
+    const id = req.params.id;
+    const update = req.body;
+    let noteIndex = notes.findIndex((note) => note.id === id);
+    if (!noteIndex) {
+      return res.status(404).json({ error: "note not found" });
+    }
+    notes[noteIndex] = { ...update, id };
 
-function getNote(req, res) {
-  const id = req.params.id;
-  const note = notes.find((note) => note.id === id);
-  if (!note) {
-    return res.status(404).json({ error: "note not found" });
-  }
-  res.json(note);
-}
+    fs.writeFileSync(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify({ notes }, null, 2)
+    );
+    res.json(notes[noteIndex]);
+  },
+  // called in .delete()
+  deleteNote(req, res) {
+    const id = req.params.id;
+    const note = notes.findIndex((note) => note.id === id);
+    // bug : last note object is deleted regardless of id
+    if (!note) {
+      return res.status(404).json({ error: "note not found" });
+    }
 
-function createNote({ body }, res) {
-  body.id = generateId();
-  const note = body;
-  notes.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
-    JSON.stringify({ notes }, null, 2)
-  );
-  res.json(note);
-}
-
-function updateNote(req, res) {
-  const id = req.params.id;
-  const update = req.body;
-  let noteIndex = notes.findIndex((note) => note.id === id);
-  if (!noteIndex) {
-    return res.status(404).json({ error: "note not found" });
-  }
-  notes[noteIndex] = { ...update, id };
-
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
-    JSON.stringify({ notes }, null, 2)
-  );
-  res.json(notes[noteIndex]);
-}
-
-function deleteNote(req, res) {
-  const id = req.params.id;
-  const note = notes.findIndex((note) => note.id === id);
-  if (!note) {
-    return res.status(404).json({ error: "note not found" });
-  }
-
-  // db.json file is rewritten by using a notes array with the specified note object removed via .splice()
-  notes.splice(note, 1);
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
-    JSON.stringify({ notes }, null, 2)
-  );
-  res.json({ message: "note deleted" });
-}
-
-module.exports = { getAllNotes, getNote, createNote, updateNote, deleteNote };
+    // db.json file is rewritten by using a notes array with the specified note object removed via .splice()
+    notes.splice(note, 1);
+    fs.writeFileSync(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify({ notes }, null, 2)
+    );
+    res.json({ message: "note deleted" });
+  },
+};
+module.exports = noteController;
