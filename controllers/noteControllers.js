94e190daf10037f553fb100b3230d01b2656c0e0
escowin <edwin@escowinart.com>
2023-06-18 11:50:34 -0500

    modularized all rest api controller & routes

--- controllers/noteControllers.js
+++ controllers/noteControllers.js
@@ -6,10 +6,19 @@ const { generateId } = require("../utils/helpers");
 // crud functions used in express routes
 function getAllNotes(req, res) {
   let result = notes;
-  res.json(result)
+  res.json(result);
 }
 
-function createNote(body, notes) {
+function getNote(req, res) {
+  const id = req.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (!note) {
+    return res.status(404).json({ error: "note not found" });
+  }
+  res.json(note);
+}
+
+function createNote({ body }, res) {
   body.id = generateId();
   const note = body;
   notes.push(note);
@@ -17,15 +26,39 @@ function createNote(body, notes) {
     path.join(__dirname, "../db/db.json"),
     JSON.stringify({ notes }, null, 2)
   );
-
-  return note;
+  res.json(note);
 }
 
-function updateNotes(notes) {
+function updateNote(req, res) {
+  const id = req.params.id;
+  const update = req.body;
+  let noteIndex = notes.findIndex((note) => note.id === id);
+  if (!noteIndex) {
+    return res.status(404).json({ error: "note not found" });
+  }
+  notes[noteIndex] = { ...update, id };
+
   fs.writeFileSync(
     path.join(__dirname, "../db/db.json"),
     JSON.stringify({ notes }, null, 2)
-  )
+  );
+  res.json(notes[noteIndex]);
 }
 
-module.exports = { getAllNotes, createNote, updateNotes };
+function deleteNote(req, res) {
+  const id = req.params.id;
+  const note = notes.findIndex((note) => note.id === id);
+  if (!note) {
+    return res.status(404).json({ error: "note not found" });
+  }
+
+  // db.json file is rewritten by using a notes array with the specified note object removed via .splice()
+  notes.splice(note, 1);
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify({ notes }, null, 2)
+  );
+  res.json({ message: "note deleted" });
+}
+
+module.exports = { getAllNotes, getNote, createNote, updateNote, deleteNote };
--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,52 +1,14 @@
-// ::route:: /api/noteRoutes
 const router = require("express").Router();
-const { getAllNotes, createNote, updateNotes } = require("../../controllers/noteControllers");
-const { notes } = require("../../db/db.json");
+const {
+  getAllNotes,
+  getNote,
+  createNote,
+  updateNote,
+  deleteNote,
+} = require("../../controllers/noteControllers");
 
-// CRUD
-// - get all notes
-router.route("/").get(getAllNotes);
-
-// - post a note. note will have an id
-router.post("/notes", (req, res) => {
-  const note = createNote(req.body, notes);
-  res.json(note);
-});
-
-router.get("/notes/:id", (req, res) => {
-  const id = req.params.id;
-  const note = notes.find((note) => note.id === id);
-  if (!note) {
-    return res.status(404).json({ error: "note not found" });
-  }
-  res.json(note);
-});
-
-// - edit a note | will require note.id
-router.put("/notes/:id", (req, res) => {
-  const id = req.params.id;
-  const update = req.body;
-  let noteIndex = notes.findIndex((note) => note.id === id);
-  if (!noteIndex) {
-    return res.status(404).json({ error: "note not found" });
-  }
-  notes[noteIndex] = { ...update, id };
-  updateNotes(notes);
-  res.json(notes[noteIndex]);
-});
-
-// - delete a note based on its id
-router.delete("/notes/:id", (req, res) => {
-  const id = req.params.id;
-  const note = notes.findIndex((note) => note.id === id);
-  if (!note) {
-    return res.status(404).json({ error: "note not found" });
-  }
-
-  // notes is updated by removing one note object through .splice(). this new notes array rewrites db.json
-  notes.splice(note, 1);
-  updateNotes(notes);
-  res.json({ message: "note deleted" });
-});
+// crud endpoints
+router.route("/").get(getAllNotes).post(createNote);
+router.route("/:id").get(getNote).put(updateNote).delete(deleteNote);
 
 module.exports = router;
